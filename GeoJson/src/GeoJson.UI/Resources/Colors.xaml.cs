569395517cbc4fddacd202013daf57cb13e740cf
Mathias Roelants <mathias.roelants@rmdy.be>
2021-02-21 15:23:36 +0100

    Added project

---
+++ GeoJson/src/GeoJson.UI/Resources/Colors.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace GeoJson.UI.Resources
+{
+    public partial class Colors : ResourceDictionary
+    {
+        public Colors()
+        {
+            InitializeComponent();
+        }
+    }
+}
